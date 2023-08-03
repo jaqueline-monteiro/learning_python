3cafdb0116aedb923bb511ca42be6846b26c2680
Jaqueline Monteiro <jaqueline.monteiro2012@gmail.com>
2023-08-03 22:02:15 +0000

    Implementing game shutdown

--- hangman.py
+++ hangman.py
@@ -1,30 +1,40 @@
 def play():
     print("*********************************")
-    print("Welcome to Hangman!")
+    print("***Welcome to the Hangman Game!***")
     print("*********************************")
 
-    secret_word = "banana"
-    guessed_letters = ["_", "_", "_", "_", "_", "_"]
+    secret_word = "apple".upper()
+    guessed_letters = ["_" for letter in secret_word]
 
     hanged = False
     guessed = False
+    errors = 0
 
     print(guessed_letters)
 
-    while(not hanged and not guessed):
+    while (not hanged and not guessed):
 
         guess = input("Which letter? ")
-        guess = guess.strip()
+        guess = guess.strip().upper()
 
-        index = 0
-        for letter in secret_word:
-            if(guess.upper() == letter.upper()):
-                guessed_letters[index] = letter
-            index = index + 1
+        if guess in secret_word:
+            index = 0
+            for letter in secret_word:
+                if guess == letter:
+                    guessed_letters[index] = letter
+                index += 1
+        else:
+            errors += 1
 
+        hanged = errors == 6
+        guessed = "_" not in guessed_letters
         print(guessed_letters)
 
+    if guessed:
+        print("You won!!")
+    else:
+        print("You lost!!")
     print("End of the game")
 
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     play()
